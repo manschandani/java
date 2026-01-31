7797914886f2db444198e2a5da5c3883afd3274b
Mansi Chandani <mansi.uttamchandani.btech2024@sitpune.edu.in>
2026-01-31 11:18:13 +0530

    new

---
+++ Main.java
@@ -0,0 +1,19 @@
+class animal{
+    void eat(){
+        System.out.println("this animal eats food");
+    }
+}
+
+class dog extends animal{
+    void bark(){
+        System.out.println("dog barks!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        dog d = new dog();
+        d.eat();
+        d.bark();
+    }
+}
