91c38c398ffe2d9d402486388c6c9908053162a9
Mansi Chandani <mansi.uttamchandani.btech2024@sitpune.edu.in>
2026-02-01 22:58:59 +0530

    new

--- Main.java
+++ Main.java
@@ -33,11 +33,29 @@ class demo{
     }
 }
 
+
+class Parent{
+    void display(){
+        System.out.println("parent class method");
+        
+    }
+}
+class Child extends Parent{
+    void display(){
+        super.display();
+        System.out.println("child class method");
+    }
+}
+
 public class Main {
     public static void main(String[] args){
         dog d = new dog();
         d.eat();
         d.bark();
         new demo();
+         
+        Child c= new Child();
+        c.display();
+    
     }
 }
