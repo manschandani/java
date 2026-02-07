219e12d2e66add6059e445c97f3b114a27faf4ac
Mansi Chandani <mansi.uttamchandani.btech2024@sitpune.edu.in>
2026-02-07 14:10:47 +0530

    new

--- assignment7.java
+++ assignment7.java
@@ -1,14 +1,41 @@
-final class finalclassexample{
+final class finalclassexample {
+
     final int finalvar = 100;
-    final void finalmethod(){
+
+    final void finalmethod() {
         System.out.println("this is a final method");
-        System.out.println("value of final variable:"+ finalvar);
+        System.out.println("value of final variable: " + finalvar);
     }
 }
-public class assignment7{
-    public static void main(String[] args){
+
+abstract class calculator {
+
+    public abstract void add(double a, double b);
+
+    public abstract void subtract(double a, double b);
+}
+
+class simplecalculator extends calculator {
+
+    public void add(double a, double b) {
+        System.out.println("addition: " + (a + b));
+    }
+
+    public void subtract(double a, double b) {
+        System.out.println("subtraction: " + (a - b));
+    }
+}
+
+public class assignment7 {
+
+    public static void main(String[] args) {
+
         finalclassexample obj = new finalclassexample();
         obj.finalmethod();
-        System.out.println("accessing final variable directly"+ obj.finalvar);
+        System.out.println("accessing final variable: " + obj.finalvar);
+
+        calculator calc = new simplecalculator();
+        calc.add(10, 5);
+        calc.subtract(10, 5);
     }
 }
